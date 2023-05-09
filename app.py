5e0468054ef2be6cf4202057d6d8da970ec43731
flintt <flintthuang@ymail.com>
2023-05-09 14:08:24 +0800

    add Readme and other small changes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, send, emit
+from flask_socketio import SocketIO, emit
 from flask import request
 app = Flask(__name__)
 
