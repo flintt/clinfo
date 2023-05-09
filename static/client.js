34052af5e66d5406a5671edc85750661338ecf23
flintt <flintthuang@ymail.com>
2023-05-09 13:24:43 +0800

    记录前加了时间戳

--- static/client.js
+++ static/client.js
@@ -9,9 +9,10 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     socket.on("client_info", (data) => {
       const { ip_address, port } = data;
+      const timestamp = new Date();
   
-      // 将新的客户端信息添加到历史记录数组中
-      history.push({ ip_address, port });
+    // 将新的客户端信息和时间戳添加到历史记录数组中
+    history.push({ ip_address, port, timestamp });
   
       // 保持历史记录数组的长度为10
       if (history.length > 10) {
@@ -21,9 +22,11 @@ document.addEventListener("DOMContentLoaded", function () {
       // 更新客户端信息的显示
       clientInfoDiv.innerHTML = history
         .map((item, index) => {
-          return `<p>${index + 1}. IP Address: ${item.ip_address}, Port: ${
-            item.port
-          }</p>`;
+          // 格式化时间戳为可读字符串
+          const timeString = item.timestamp.toLocaleString();
+          return `<p>${index + 1}. (${timeString}) IP Address: ${
+            item.ip_address
+          }, Port: ${item.port}</p>`;
         })
         .join("");
     });
